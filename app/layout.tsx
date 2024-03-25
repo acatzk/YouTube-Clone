f9e8978a88c55582f96f2a751d92b62fb6e49acf
Joshua Galit <joshua@everywatch.com>
2024-03-25 21:51:50 +0800

    feat: setup shadcn-ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import { ReactNode } from 'react'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter as FontSans } from 'next/font/google'
 
 import '~/styles/globals.css'
+import { cn } from '~/lib/utils'
 
-const inter = Inter({ subsets: ['latin'] })
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans'
+})
 
 export const metadata: Metadata = {
   title: 'Youtube API',
@@ -18,7 +22,7 @@ type RootLayoutProps = {
 export default function RootLayout({ children }: Readonly<RootLayoutProps>): JSX.Element {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn('min-h-screen bg-slate-100', fontSans.variable)}>{children}</body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from '~/components/ui/button'
+
 export default function Home(): JSX.Element {
-  return <div className="font-bold text-2xl">Hello World</div>
+  return (
+    <div className="font-bold text-2xl">
+      <Button variant="destructive">Hello</Button>
+    </div>
+  )
 }
